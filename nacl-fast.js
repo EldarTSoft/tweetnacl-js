16555baa078479c80f999c3f525984f4b36c7bef
EldarTSoft <eldar.tinjic@softatec.com>
2017-05-23 12:48:22 +0200

    Update nacl-fast.js

--- nacl-fast.js
+++ nacl-fast.js
@@ -2347,30 +2347,17 @@ nacl.setPRNG = function(fn) {
 };
 
 (function() {
-  // Initialize PRNG if environment provides CSPRNG.
-  // If not, methods calling randombytes will throw.
-  var crypto = typeof self !== 'undefined' ? (self.crypto || self.msCrypto) : null;
-  if (crypto && crypto.getRandomValues) {
-    // Browsers.
-    var QUOTA = 65536;
-    nacl.setPRNG(function(x, n) {
-      var i, v = new Uint8Array(n);
-      for (i = 0; i < n; i += QUOTA) {
-        crypto.getRandomValues(v.subarray(i, i + Math.min(n - i, QUOTA)));
-      }
-      for (i = 0; i < n; i++) x[i] = v[i];
-      cleanup(v);
-    });
-  } else if (typeof require !== 'undefined') {
-    // Node.js.
-    crypto = require('crypto');
-    if (crypto && crypto.randomBytes) {
-      nacl.setPRNG(function(x, n) {
-        var i, v = crypto.randomBytes(n);
-        for (i = 0; i < n; i++) x[i] = v[i];
-        cleanup(v);
-      });
-    }
+  
+  let crypto
+  if (typeof window === 'object') {
+    if (!window.crypto) window.crypto = {}
+    crypto = window.crypto
+  } else {
+    crypto = require('crypto')
+  }
+
+  if (!crypto.getRandomValues) {
+    crypto.getRandomValues = getRandomValues
   }
 })();
 
